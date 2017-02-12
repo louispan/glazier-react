f606264966fcf79c9be9ed85299b10a9b80e1942
Louis Pan <louis@pan.me>
2017-02-12 15:02:52 +1100

    combineElements

--- src/Glazier/React/Window.hs
+++ src/Glazier/React/Window.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Window here refers to 'Glazier.Window' (ie. rendering View of state), not browser window
 module Glazier.React.Window
     ( ReactElement(..)
     , createElement
+    , combineElements
     ) where
 
-import GHCJS.Types (JSString, JSVal, IsJSVal, jsval)
 import GHCJS.Marshal.Pure (PToJSVal(..))
-import JavaScript.Array (JSArray)
+import GHCJS.Types (IsJSVal, JSString, JSVal, jsval)
+import JavaScript.Array (JSArray, fromList)
 
 -- | The constructor for ReactElement is exported for convenient interoperabilithy with foreign
 -- react components. It is assumed that it is only constructed for JSVal that have been created
@@ -20,7 +23,13 @@ instance PToJSVal ReactElement where
 
 foreign import javascript unsafe
   "$r = React.createElement($1, $2, $3);"
-  js_createElement :: JSString -> JSVal -> JSArray -> IO ReactElement
+  js_createElement :: JSString -> JSVal -> JSArray -> ReactElement
 
-createElement :: JSString -> JSVal -> JSArray -> IO ReactElement
+createElement :: JSString -> JSVal -> JSArray -> ReactElement
 createElement = js_createElement
+
+
+-- | React only allows a single top most element.
+-- Provide a handly function to wrap a list of ReactElements inside a 'div'
+combineElements :: JSVal -> [ReactElement] -> ReactElement
+combineElements props xs = js_createElement "div" props (fromList $ jsval <$> xs)
