28a93b3d2a7626739fb580229665c01a5cc01bf4
Louis Pan <louis@pan.me>
2017-02-15 21:37:16 +1100

    removed debug logging

--- examples/todo/src/App.js
+++ examples/todo/src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {registry, combineElements} from '../build/todo.min.js';
-
-console.log(registry, combineElements);
+import {registry, combineElements} from '../build/todo.min';
 
 class App extends Component {
 
