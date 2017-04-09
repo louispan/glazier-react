b60664803e80cac966db85a83d2cb5bd3278983b
Louis Pan <louis@lory.local>
2017-04-09 15:41:59 +1000

    fixed minified bugs

--- jsbits/react.js
+++ jsbits/react.js
@@ -6,29 +6,30 @@
 //     "react-dom": "^15.4.2"
 //   }
 // }
-var hgr$React_;
+var hgr$React_ = null;
 function hgr$React() {
-    if (typeof hgr$React_ == "undefined") {
+    if (!hgr$React_) {
         hgr$React_ = require('react');
     }
     return hgr$React_;
 }
 
-var hgr$ReactDOM_;
+var hgr$ReactDOM_ = null;
 function hgr$ReactDOM() {
-    if (typeof hgr$ReactDOM_ == "undefined") {
+    if (!hgr$ReactDOM_) {
         hgr$ReactDOM_ = require('react-dom');
     }
     return hgr$ReactDOM_;
 }
 
-var hgr$shimComponent_;
+var hgr$shimComponent_ = null;
 function hgr$shimComponent() {
-    if (typeof hgr$shimComponent_ == "undefined") {
+    if (!hgr$shimComponent_) {
         // Inheriting from Component means every call to this.setState will result in a render
         // Inheriting from PureComponet means a shallow comparison will be made
-        var React = hgr$React();
-        class Shim extends React.PureComponent {
+        // Protect "PureComponent" from closure compiler because it's not in the official externs
+        var ReactPureComponent = hgr$React()["PureComponent"];
+        class Shim extends ReactPureComponent {
 
             componentDidUpdate() {
                 if (this.props['componentDidUpdate'])
