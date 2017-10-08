7c5378016fe6fde8380bffda3141a78bf427da77
Louis Pan <louis@lory.lan>
2017-10-09 07:05:38 +1100

    mkCallback explicitly accepts IO trigger and IO executor, but only ReactorMonad handler

--- src/Glazier/React/Reactor.hs
+++ src/Glazier/React/Reactor.hs
@@ -1,22 +1,28 @@
 module Glazier.React.Reactor where
 
+import Control.DeepSeq
+import Control.Monad.Trans.Maybe
 import Data.IORef
 import qualified Data.JSString as J
 import qualified GHCJS.Foreign.Callback as J
 import qualified GHCJS.Types as J
 import qualified Glazier.React.Component as R
 import qualified Glazier.React.Markup as R
-import qualified Glazier.React.Dispose as R
 
-class Monad m => MonadReactor m where
+class Monad m =>
+      MonadReactor m where
     doNewIORef :: a -> m (IORef a)
     doReadIORef :: IORef a -> m a
     doWriteIORef :: IORef a -> a -> m ()
-    mkCallback :: (J.JSVal -> m ()) -> m (J.Callback (J.JSVal -> IO ()))
+    mkCallback
+        :: (Foldable t1, Foldable t2, NFData (t1 a))
+        => (J.JSVal -> MaybeT IO (t1 a))
+        -> (a -> MaybeT m (t2 c))
+        -> (c -> MaybeT IO ())
+        -> m (J.Callback (J.JSVal -> IO ()))
     mkRenderer :: R.ReactMlT m () -> m (J.Callback (IO J.JSVal))
     getComponent :: m R.ReactComponent
     mkKey :: m Int
-    doDispose :: R.Disposable () -> m ()
 
 mkKey' :: MonadReactor m => m J.JSString
 mkKey' = (J.pack . show) <$> mkKey
--- src/Glazier/React/Reactor/Run.hs
+++ src/Glazier/React/Reactor/Run.hs
@@ -4,13 +4,16 @@
 
 module Glazier.React.Reactor.Run where
 
+import Control.Monad.Trans.Maybe
 import Control.Monad.Reader
 import Data.IORef
+import Data.Foldable
+import Control.Monad.Morph
 import qualified GHCJS.Foreign.Callback as J
 import qualified Glazier.React.Component as R
-import Glazier.React.Reactor as R
+import qualified Glazier.React.Event as R
 import qualified Glazier.React.Markup as R
-import qualified Glazier.React.Dispose as R
+import Glazier.React.Reactor as R
 import JavaScript.Extras as JE
 
 newtype IOReactor a = IOReactor
@@ -26,9 +29,12 @@ instance MonadReactor IOReactor where
     doNewIORef = liftIO . newIORef
     doReadIORef = liftIO . readIORef
     doWriteIORef v a = liftIO $ writeIORef v a
-    mkCallback f = do
+    mkCallback goStrict goLazy exec = do
         env <- ask
-        liftIO $ J.syncCallback1 J.ContinueAsync (\j -> runReaderT (runIOReactor (f j)) env)
+        let goLazy' a = hoist (\m -> runReaderT (runIOReactor m) env) (goLazy a)
+            goLazy'' a = goLazy' a >>= traverse_ exec
+            f = R.handleEventM goStrict (traverse_ goLazy'')
+        liftIO $ J.syncCallback1 J.ContinueAsync (void . runMaybeT . f)
     mkRenderer rnd = do
         env <- ask
         liftIO $ J.syncCallback' (runReaderT (runIOReactor (JE.toJS <$> R.toElement rnd)) env)
@@ -40,4 +46,3 @@ instance MonadReactor IOReactor where
         i <- liftIO $ readIORef v
         liftIO $ modifyIORef' v (\j -> (j `mod` JE.maxSafeInteger) + 1)
         pure i
-    doDispose = liftIO . R.runDisposable
