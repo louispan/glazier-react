a98a1117f17f989eed0d486850448dcdd1dc134f
Louis Pan <louis@pan.me>
2017-12-10 09:09:09 +1100

    Added JE instances for Renderer and ReactKey

--- src/Glazier/React/Reactor.hs
+++ src/Glazier/React/Reactor.hs
@@ -5,14 +5,19 @@ module Glazier.React.Reactor where
 import Control.DeepSeq
 import Data.IORef
 import qualified Data.JSString as J
+import Data.String
 import qualified GHCJS.Foreign.Callback as J
+import qualified GHCJS.Marshal.Pure as J
 import qualified GHCJS.Types as J
 import qualified Glazier.React.Component as R
 import qualified Glazier.React.Dispose as R
 import qualified Glazier.React.Markup as R
+import JavaScript.Extras.Cast as JE
 
-newtype Renderer = Renderer { runRenderer :: J.Callback (IO J.JSVal) } deriving R.Dispose
-newtype ReactKey = ReactKey { runReactKey :: J.JSString } deriving (Read, Show, Eq, Ord, R.Dispose)
+newtype Renderer = Renderer { runRenderer :: J.Callback (IO J.JSVal) }
+    deriving (R.Dispose, JE.ToJS, J.IsJSVal)
+newtype ReactKey = ReactKey { runReactKey :: J.JSString }
+    deriving (Read, Show, Eq, Ord, R.Dispose, JE.ToJS, JE.FromJS, IsString, J.IsJSVal, J.PToJSVal)
 
 class Monad m =>
       MonadReactor m where
