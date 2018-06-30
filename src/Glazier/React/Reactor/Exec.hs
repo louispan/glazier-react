de2f98289e30745e6784abe855a5f5d064b18bf9
Louis Pan <louis@pan.me>
2018-06-30 23:55:58 +1000

    fixed not putting mvar bug

--- src/Glazier/React/Reactor/Exec.hs
+++ src/Glazier/React/Reactor/Exec.hs
@@ -204,6 +204,7 @@ doRef scnRef scnVar j = do
 
 doRendered :: IORef (Scene s) -> MVar (Scene s) -> IO ()
 doRendered scnRef scnVar = do
+    putStrLn "rendered"
     -- update nextRenderedListener held in the Plan
     scn <- takeMVar scnVar
     let scn' = scn & _plan._nextRenderedListener .~ mempty
@@ -376,6 +377,7 @@ scheduleRerender sbj = do
     scn <- takeMVar scnVar
     if scn ^. _plan._rerenderRequired
         then do
+            putStrLn "render required" -- FIXME
             let scn' = scn & _plan._rerenderRequired .~ False
                         & _plan._tickedNotified .~ False
             -- Update the back buffer
@@ -383,9 +385,13 @@ scheduleRerender sbj = do
             putMVar scnVar scn'
             case scn ^. _plan._componentRef of
                 Nothing -> pure ()
-                Just j -> rerenderShim j
+                Just j -> do
+                    putStrLn "rerendering" -- FIXME
+                    rerenderShim j
         -- rerender not required (eg. already processed)
-        else putMVar scnVar scn
+        else do
+            putStrLn "render not notified" -- FIXME
+            putMVar scnVar scn
   where
     scnRef = sceneRef sbj
     scnVar = sceneVar sbj
@@ -403,15 +409,18 @@ execTickModel ::
 execTickModel sbj tick = do
     q <- view ((hasLens @ReactorEnv)._reactorBackgroundEnv)
     liftIO $ do
-        putStrLn "execTickMode" -- FIXME
+        putStrLn "execTickMode1" -- FIXME
         scn <- takeMVar scnVar
         let s = scn ^. _model
         (c, s') <- unReadIORef $ runStateT tick s
         let scn' = scn & _model .~ s'
         -- Update the back buffer
+        putStrLn "execTickMode2" -- FIXME
         atomicWriteIORef scnRef scn'
         putMVar scnVar scn'
+        putStrLn "execTickMode3" -- FIXME
         atomically $ writeTQueue q notifyTicked
+        putStrLn "execTickMode4" -- FIXME
         pure c
   where
     scnRef = sceneRef sbj
@@ -420,6 +429,7 @@ execTickModel sbj tick = do
         scn <- takeMVar scnVar
         if not (scn ^. _plan._tickedNotified)
             then do
+                putStrLn "tick not notified" -- FIXME
                 let scn' = scn & _plan._tickedNotified .~ True
                         & _plan._rerenderRequired .~ True
                     cb = scn ^. _plan._tickedListener
@@ -431,6 +441,7 @@ execTickModel sbj tick = do
                 pure (scheduleRerender sbj)
             -- notify not required (eg. already processed)
             else do
+                putStrLn "tick already notified" -- FIXME
                 putMVar scnVar scn
                 pure (pure ())
 
@@ -520,13 +531,13 @@ execGetElementalRef executor sbj ri k = do
                 putMVar scnVar scn'
             -- trigger a rerender
             execRerender sbj
-    case (refFreshness, ret) of
-        (Fresh, _) -> doTryAgain
-        (_, Nothing) -> doTryAgain
-        _ -> pure ()
-    case ret of
-        Nothing -> pure ()
-        Just ret' -> executor . k $ ret'
+    case refFreshness of
+        Fresh -> doTryAgain
+        Existing -> case ret of
+            Nothing -> doTryAgain
+            Just ret' -> do
+                liftIO $ putMVar scnVar scn
+                executor . k $ ret'
   where
     scnRef = sceneRef sbj
     scnVar = sceneVar sbj
