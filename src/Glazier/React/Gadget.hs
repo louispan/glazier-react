7bd2ccdb04c01de1e21e8393aad643ded308815c
Louis Pan <louis@pan.me>
2019-04-21 17:00:21 +1000

    subjec wip

--- src/Glazier/React/Gadget.hs
+++ src/Glazier/React/Gadget.hs
@@ -9,18 +9,20 @@ import Control.Monad.Trans.Reader
 import Glazier.Command
 import Glazier.Logger
 import Glazier.React.Obj
-import Glazier.React.Subject
+-- import Glazier.React.Subject
 
--- type MonadGadget s r c m = (MonadReactor c m, HasWeakObj s r, Has ReactId r)
+type MonadGadget c r o m = (MonadReactor c m, HasWeakObj o r)
+
+type MonadGadget' c r o m = (MonadReactor c m, HasWeakObj o r, Has ReactId r)
 
 -- | A 'Gadget' is an instance of 'MonadReactor'
 -- The @s@ state can be magnified with 'magnifiedSubject'
-type Gadget c o s = ReaderT (Subject o s) (ContT () (Program c))
+type Gadget c r = ReaderT r (ContT () (Program c))
 
-instance MonadLogLevel (Gadget c o s) where
+instance GetWeakObj s r => HMonadLogLevel (Gadget c r) where
     -- logLevel :: m (Benign IO (Maybe LogLevel))
     logLevel = do
-        obj <- view (to self)
+        obj <- view _getWeakObj
         pure $ go obj
       where
         go obj = runMaybeT $ do
@@ -28,26 +30,25 @@ instance MonadLogLevel (Gadget c o s) where
             s <- MaybeT $ benignDeRefWeak r
             pure . planLogLevel . plan $ s
 
--- toGadget ::
---     (Entity o s
---         -> (a -> Program c ())
---         -> Program c ())
---     -> Gadget c o s a
--- toGadget f = ReaderT (\r -> ContT (f r))
+toGadget ::
+    (r
+        -> (a -> Program c ())
+        -> Program c ())
+    -> Gadget c r a
+toGadget f = ReaderT (\r -> ContT (f r))
 
--- runGadget ::
---     Gadget c o s a
---     -> Entity o s
---     -> (a -> Program c ())
---     -> Program c ()
--- runGadget x l = runContT (runReaderT x l)
+runGadget ::
+    Gadget c r a
+    -> r
+    -> (a -> Program c ())
+    -> Program c ()
+runGadget x l = runContT (runReaderT x l)
 
--- wack :: Gadget c o s a -> Gadget c o s a -> Gadget c o s a
--- wack = also
+-- gadgetWith :: GetWeakObj o r => o -> Gadget c (WeakObj o) a -> ContT () (Program c) a
+-- gadgetWith obj = (`runReaderT` (_getWeakObj obj))
 
+-- gadgetWith :: GetWeakObj o r => o -> Gadget c (WeakObj o, ReactId) a -> ContT () (Program c) a
+-- gadgetWith obj = (`runReaderT` (Subject id (_getWeakObj obj)))
 
--- gadgetWith :: GetWeakObj o s => o -> Gadget c s s a -> ContT () (Program c) a
--- gadgetWith obj = (`runReaderT` (Entity id (weakObj obj)))
-
--- evalGadget :: Gadget c o s () -> Entity o s -> (Program c) ()
--- evalGadget gad ent = evalContT . (`runReaderT` ent) $ gad
+evalGadget :: Gadget c r () -> r -> (Program c) ()
+evalGadget gad r = evalContT . (`runReaderT` r) $ gad
--- src/Glazier/React/Obj.hs
+++ src/Glazier/React/Obj.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Glazier.React.Obj
 ( WeakObj
@@ -22,18 +23,18 @@ module Glazier.React.Obj
 , benignReadObjScene
 ) where
 
+-- import Data.Diverse.Lens
 import Control.Lens
 import Control.Monad.Trans.Maybe
-import Data.Diverse.Lens
 import Glazier.Benign
 import Glazier.React.Obj.Internal
 import Glazier.React.Scene
 
--- -- | Get a 'Lens' to a 'WeakObj'.
--- -- The 'Setter' in the 'Lens' is useful to be used in a 'MonadReader' environment
--- -- so that it is easy to run a 'MonadRader' for a different 'WeakObj'
--- -- without knowing anything else about the environment.
--- -- The @r -> o@ functional dependency resolves ambiguity with @o@
+-- | Get a 'Lens' to a 'WeakObj'.
+-- The 'Setter' in the 'Lens' is useful to be used in a 'MonadReader' environment
+-- so that it is easy to run a 'MonadRader' for a different 'WeakObj'
+-- without knowing anything else about the environment.
+-- The @r -> o@ functional dependency resolves ambiguity with @o@
 -- class (Has (WeakObj s) r) => HasWeakObj s r | r -> s where
 --     _weakObj :: Lens' r (WeakObj s)
 --     _weakObj = hasLens @(WeakObj s)
@@ -50,6 +51,9 @@ import Glazier.React.Scene
 -- The @r -> o@ functional dependency resolves ambiguity with @o@
 class GetWeakObj s r | r -> s where
     _getWeakObj :: Getter r (WeakObj s)
+
+-- instance {-# OVERLAPPABLE #-} HasWeakObj s r => GetWeakObj s r where
+--     _getWeakObj = _weakObj
 
 instance GetWeakObj s (WeakObj s) where
     _getWeakObj = id
