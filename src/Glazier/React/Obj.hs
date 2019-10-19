b1f7bb2d91e2572f4db4ebeced7a349c412fc9ba
Louis Pan <louis@pan.me>
2019-10-20 06:32:43 +1100

    Added Obj getting lens

--- src/Glazier/React/Obj.hs
+++ src/Glazier/React/Obj.hs
@@ -2,13 +2,17 @@ module Glazier.React.Obj
     ( -- | constructor not exported
       Ref
     , strongRef
+    , _strongRef
     , weakRef
+    , _weakRef
        -- | constructor not exported
     , PlanRef
     , ModelVar
     , Obj
     , modelVar
+    , _modelVar
     , planRef
+    , _planRef
     ) where
 
 import Glazier.React.Obj.Internal
--- src/Glazier/React/Obj/Internal.hs
+++ src/Glazier/React/Obj/Internal.hs
@@ -4,6 +4,7 @@
 module Glazier.React.Obj.Internal where
 
 import Control.Concurrent.MVar
+import Control.Lens
 import Data.IORef
 import qualified GHC.Generics as G
 import Glazier.React.Plan
@@ -16,8 +17,14 @@ data Ref v a = Ref (v a) (Weak (v a)) deriving (G.Generic)
 strongRef :: Ref v a -> v a
 strongRef (Ref s _) = s
 
+_strongRef :: (Profunctor p, Contravariant f) => Optic' p f (Ref v a) (v a)
+_strongRef = to strongRef
+
 weakRef :: Ref v a -> Weak (v a)
 weakRef (Ref _ w) = w
+
+_weakRef :: (Profunctor p, Contravariant f) => Optic' p f (Ref v a) (Weak (v a))
+_weakRef = to weakRef
 
 type PlanRef = Ref IORef Plan
 type ModelVar s = Ref MVar s
@@ -28,5 +35,11 @@ data Obj s = Obj PlanRef (ModelVar s)
 modelVar :: Obj s -> ModelVar s
 modelVar (Obj _ s) = s
 
+_modelVar :: (Profunctor p, Contravariant f) => Optic' p f (Obj s) (ModelVar s)
+_modelVar = to modelVar
+
 planRef :: Obj s -> PlanRef
 planRef (Obj p _) = p
+
+_planRef :: (Profunctor p, Contravariant f) => Optic' p f (Obj s) PlanRef
+_planRef = to planRef
